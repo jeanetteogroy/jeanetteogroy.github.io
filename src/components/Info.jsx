0a7f94ec532ac3c47d12b66190ba67a3eacf0891
nicolmol <56063595+nicolaimol@users.noreply.github.com>
2023-01-02 11:32:14 +0100

    skrivefeil

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -33,7 +33,7 @@ export default function Info() {
 
     return (
         <div style={{display: 'flex', flexDirection: 'column' }}>
-            <Typography variant="h6">Gaveønsker fra Tilbord</Typography>
+            <Typography variant="h6">Gaveønsker fra Tilbords</Typography>
             <Typography> <Link href="https://www.tilbords.no/bryllup/vis-liste/105283">Link her</Link></Typography>
             <div style={{display: 'flex', flexDirection: 'row', gap: '2em'}}>
 
